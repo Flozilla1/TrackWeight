26156e10382a7f20ca4fdeec81a34b1750e794db
Florian Schäfer <>
2025-05-26 14:56:25 +0200

    test: trigger appwrite function via api

--- appwriteFunctions/src/main.py
+++ appwriteFunctions/src/main.py
@@ -1,5 +1,2 @@
 def main(context):
-    if context.req.method == "POST":
-        return context.res.json({"message": "hello world", "req": context.req})
-    else:
-        return context.res.json({"error": "Method Not Allowed"}, status_code=405)
+    return context.res.json({"message": "hello world", "req": context.req})
